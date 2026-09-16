374608442de32a02d24540196fb249b1b0f69bb6
shajugit <v.shajahan1996@gmail.com>
2026-09-16 12:22:48 +0530

    jenkins

--- vars/testPipeline.groovy
+++ vars/testPipeline.groovy
@@ -1,10 +1,16 @@
 def call (Map configMap) {
     pipeline {
         agent any
+        environment {
+            project = configMap.project
+            component = configMap.component
+        }
         stages {
             stage('Test') {
                 steps {
                     echo 'This is a test pipeline.'
+                    echo "Project: ${project}"
+                    echo "Component: ${component}"
                 }
             }
         }
